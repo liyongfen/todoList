0822b0a7095db5a47b33cd39d0fe43241ffeb22e
liyongfen <liyb@yonyou.com>
2017-06-16 19:29:46 +0800

    add antd

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,31 @@
 require('../styles/main.css');
 
 import React from "react";
-import Header from "./Header.js"
+import ReactDOM from 'react-dom';  
+import { Button,DatePicker} from 'antd';
+
+import 'antd/dist/antd.css'; 
+import Header from "./Header.js";
+
 
 class App extends React.Component{
 	render(){
+		var todoListData = [{title:"户外健身1",desc:"5公里短跑，20个俯卧撑，10个引体向上，50个仰卧起坐",time:"2017/6/16",importance:"1"},
+					{title:"户外健身2",desc:"5公里短跑，20个俯卧撑，10个引体向上，50个仰卧起坐",time:"2017/6/16",importance:"1"},
+					{title:"户外健身3",desc:"5公里短跑，20个俯卧撑，10个引体向上，50个仰卧起坐",time:"2017/6/16",importance:"1"},
+					{title:"户外健身4",desc:"5公里短跑，20个俯卧撑，10个引体向上，50个仰卧起坐",time:"2017/6/16",importance:"1"}]
+		var todoListArr = [];
+		todoListData.forEach(function(value,index){
+			todoListArr.push(<li key={index}><span>{index+1}.</span> {value.title}</li>);
+		})
 		return (
 			<div className="content">
 				<Header />
-				<div className="">
-
+				<div className="main">
+					<div className="main-logo"></div>
+					<ul>{todoListArr}</ul>
+					<Button type="primary">Primary</Button>
+					<DatePicker/>
 				</div>
 				<div className="footer"></div>
 			</div>
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015', 'react']
+                    presets: ['es2015', 'react'],
+                    plugins: [['import', {"libraryName": "antd", "style": "css"}]]
                 }
             },{
                 test: /\.css$/,
